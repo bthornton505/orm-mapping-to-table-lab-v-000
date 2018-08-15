54e09f7c67725f08ccdb573425cb5ec3c2ef12c2
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-15 13:27:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,12 @@
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  attr_accessor 
   
 end
+
+
+
+
+
+
+# Remember, you can access your database connection anywhere in this class
+  #  with DB[:conn]  
