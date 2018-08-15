a9dcc66f8c7c0d0a152791ae8cdb6467e42fb7b3
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-15 13:49:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,10 @@ class Student
     sql = <<-SQL
       INSERT INTO students (name, grade) VALUES (?, ?) 
       SQL
+      
     DB[:conn].execute(sql, self.name, self.grade)
+    
+    @id = db 
   end 
   
 end
