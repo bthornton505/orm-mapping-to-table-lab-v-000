c4bfda3292d3bf37a55409b459381e466757d1ad
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-15 13:50:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
       
     DB[:conn].execute(sql, self.name, self.grade)
     
-    @id = DB[:conn].execute("SELECT LAST")
+    @id = DB[:conn].execute("SELECT last_insert_row")
   end 
   
 end
